9125950f486e38bd67b5796b2f33522540b1a0c2
Yeyner Arenales <yeyner.arenales0811@gmail.com>
2022-01-19 16:46:45 -0400

    [FEATURE][SERVICE] create info and model of user

---
+++ src/app/models/models.ts
@@ -0,0 +1,9 @@
+export interface user {
+    id: number,
+    name: string,
+    lastname: string,
+    age: number,
+    phoneNumber: number,
+    engineering: string,
+    experienceYear: number
+}
---
+++ src/assets/infoData.ts
@@ -0,0 +1,94 @@
+import { user } from "src/app/models/models"
+
+export var infoData: user[] = [
+    {
+        id: 1, name: 'Pedro', lastname: 'Ramirez', age: 25, phoneNumber: 4147316232, engineering: 'Informatica', experienceYear: 1
+    },
+    {
+        id: 2, name: 'Luis', lastname: 'Lopez', age: 27, phoneNumber: 4126596898, engineering: 'Civil', experienceYear: 2
+    },
+    {
+        id: 3, name: 'Jose', lastname: 'Perez', age: 22, phoneNumber: 7596235458, engineering: 'Industrial', experienceYear: 1
+    },
+    {
+        id: 4, name: 'Andres', lastname: 'Pereira', age: 28, phoneNumber: 4147215898, engineering: 'Mecanica', experienceYear: 3
+    },
+    {
+        id: 5, name: 'Maria', lastname: 'Zambrano', age: 26, phoneNumber: 4735689745, engineering: 'Civil', experienceYear: 3
+    },
+    {
+        id: 6, name: 'Camila', lastname: 'Martinez', age: 23, phoneNumber: 4125869874, engineering: 'Mecanica', experienceYear: 1
+    },
+    {
+        id: 7, name: 'Fernanda', lastname: 'Perez', age: 30, phoneNumber: 1536895745, engineering: 'Civil', experienceYear: 2
+    },
+    {
+        id: 8, name: 'Andrea', lastname: 'Diaz', age: 22, phoneNumber: 7598463214, engineering: 'Informatica', experienceYear: 2
+    },
+    {
+        id: 9, name: 'Juan', lastname: 'Chacon', age: 21, phoneNumber: 8956451257, engineering: 'Informatica', experienceYear: 1
+    },
+    {
+        id: 10, name: 'Johana', lastname: 'Gomez', age: 29, phoneNumber: 4147316232, engineering: 'Civil', experienceYear: 4
+    },
+    {
+        id: 11, name: 'Antonio', lastname: 'Zambrano', age: 23, phoneNumber: 5876953265, engineering: 'Mecanica', experienceYear: 2
+    },
+    {
+        id: 12, name: 'Maria', lastname: 'Gomez', age: 24, phoneNumber: 4752365984, engineering: 'Industrial', experienceYear: 2
+    },
+    {
+        id: 13, name: 'Mercedes', lastname: 'Ramirez', age: 22, phoneNumber: 4586957845, engineering: 'Civil', experienceYear: 1
+    },
+    {
+        id: 14, name: 'Freddy', lastname: 'Villamizar', age: 32, phoneNumber: 4165896232, engineering: 'Informatica', experienceYear: 5
+    },
+    {
+        id: 15, name: 'Jorge', lastname: 'Ramirez', age: 22, phoneNumber: 4145876958, engineering: 'Mecanica', experienceYear: 2
+    },
+    {
+        id: 16, name: 'Andres', lastname: 'Perez', age: 28, phoneNumber: 1453658421, engineering: 'Industrial', experienceYear: 3
+    },
+    {
+        id: 17, name: 'Pedro', lastname: 'Villamizar', age: 27, phoneNumber: 4126589536, engineering: 'Mecanica', experienceYear: 4
+    },
+    {
+        id: 18, name: 'Valentina', lastname: 'Ramirez', age: 19, phoneNumber: 4126859424, engineering: 'Informatica', experienceYear: 1
+    },
+    {
+        id: 19, name: 'Carlos', lastname: 'Marquez', age: 32, phoneNumber: 4589623565, engineering: 'Civil', experienceYear: 5
+    },
+    {
+        id: 20, name: 'Jose', lastname: 'Zambrano', age: 20, phoneNumber: 2596845798, engineering: 'Civil', experienceYear: 2
+    },
+    {
+        id: 21, name: 'Pedro', lastname: 'Perez', age: 29, phoneNumber: 2569456898, engineering: 'Industrial', experienceYear: 3
+    },
+    {
+        id: 22, name: 'Maria', lastname: 'Flores', age: 23, phoneNumber: 1456895745, engineering: 'Informatica', experienceYear: 2
+    },
+    {
+        id: 23, name: 'Alexandra', lastname: 'Perez', age: 27, phoneNumber: 5896453264, engineering: 'Mecanica', experienceYear: 3
+    },
+    {
+        id: 24, name: 'Fernanda', lastname: 'Cardenas', age: 22, phoneNumber: 1456985789, engineering: 'Industrial', experienceYear: 1
+    },
+    {
+        id: 25, name: 'Sara', lastname: 'Gonzalez', age: 23, phoneNumber: 5896459874, engineering: 'Mecanica', experienceYear: 2
+    },
+    {
+        id: 26, name: 'Andrea', lastname: 'Cardenas', age: 33, phoneNumber: 5874126987, engineering: 'Civil', experienceYear: 5
+    },
+    {
+        id: 27, name: 'Jose', lastname: 'Perdomo', age: 28, phoneNumber: 25634895465, engineering: 'Civil', experienceYear: 3
+    },
+    {
+        id: 28, name: 'Miguel', lastname: 'Ramirez', age: 20, phoneNumber: 2569874585, engineering: 'Informatica', experienceYear: 1
+    },
+    {
+        id: 29, name: 'Antonio', lastname: 'Cardenas', age: 27, phoneNumber: 4158796598, engineering: 'Industrial', experienceYear: 4
+    },
+    {
+        id: 30, name: 'Fiorella', lastname: 'Camargo', age: 22, phoneNumber: 5896783265, engineering: 'Informatica', experienceYear: 1
+    }
+]
