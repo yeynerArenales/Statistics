49bb314d412acb78b0810b73c42a6c176f23830a
Yeyner Arenales <yeyner.arenales0811@gmail.com>
2022-01-19 17:41:19 -0400

    [FEATURE][SERVICE] add length for paginated

--- src/app/services/info.service.ts
+++ src/app/services/info.service.ts
@@ -9,7 +9,12 @@ export class InfoService {
 
   constructor() { }
 
-  getInfo(): user[]{
+  getInfo(length: number): user[]{
+    let response: user[] = [];
+    for (let index = 0; index < length; index++) {
+      const element = infoData[index];
+      response.push(element)
+    }
     return infoData;
   }
 }
