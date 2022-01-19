5282b6c6f0d7001b781c8d551f2df401ea632164
Yeyner Arenales <yeyner.arenales0811@gmail.com>
2022-01-19 16:48:36 -0400

    [FEATURE][SERVICE] create function to get data

--- src/app/services/info.service.ts
+++ src/app/services/info.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import { user } from '../models/models';
+import { infoData } from 'src/assets/infoData';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +8,8 @@ import { Injectable } from '@angular/core';
 export class InfoService {
 
   constructor() { }
+
+  getInfo(): user[]{
+    return infoData;
+  }
 }
